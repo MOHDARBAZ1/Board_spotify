56ff820d11a115a859c63118cfbf6d7581566230
MOHDARBAZ1 <53903338+MOHDARBAZ1@users.noreply.github.com>
2021-04-02 11:45:55 +0530

    Add files via upload

---
+++ build/precache-manifest.ef3ddaf091de54d97a9e4100d0832fb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5adc240b3619d1d0905a8a0d65cc000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88dd90d198fe31396c37",
+    "url": "/static/css/main.cefa10a7.chunk.css"
+  },
+  {
+    "revision": "68040b176449bcce29fb",
+    "url": "/static/js/2.b0e5bc93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b0e5bc93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88dd90d198fe31396c37",
+    "url": "/static/js/main.dbff7729.chunk.js"
+  },
+  {
+    "revision": "6ab498e158b856703d50",
+    "url": "/static/js/runtime-main.6ab28b98.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
